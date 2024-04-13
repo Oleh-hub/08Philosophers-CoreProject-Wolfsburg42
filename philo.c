d697e5b4f4b57347ee803acbcad03a82c28aac7e
Oleh Ruban <oruban@c2s19d121.42wolfsburg.de>
2024-04-13 10:08:36 +0200

    norminetted

--- philo.c
+++ philo.c
@@ -6,13 +6,13 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 15:14:18 by oruban            #+#    #+#             */
-/*   Updated: 2024/04/12 20:41:48 by oruban           ###   ########.fr       */
+/*   Updated: 2024/04/13 10:05:40 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void ft_printf_out(long int time, t_philo *philo, char *str)
+static void	ft_printf_out(long int time, t_philo *philo, char *str)
 {
 	pthread_mutex_lock(&(philo->args->print_mid));
 	printf("%ld %d %s\n", time, philo->id, str);
@@ -20,7 +20,7 @@ static void ft_printf_out(long int time, t_philo *philo, char *str)
 }
 
 // returns time, passed sincse 1.01.1970 in ms
-static long int time_ms(void)
+static long int	time_ms(void)
 {
 	struct timeval	tv;
 
@@ -30,17 +30,17 @@ static long int time_ms(void)
 
 // sleeps for the time, indicated by variable time in ms
 // usleep > 10 leads 2 ft_msleep  delays, 1 ms for every 200 ms twice in 5 cases
-void ft_msleep(long int time) 
+void	ft_msleep(long int time)
 {
 	long int	start;
 
 	start = time_ms();
 	while (1)
-    {
-        usleep(10);
-        if (time_ms() - start >= time)
-            break;
-    }
+	{
+		usleep(10);
+		if (time_ms() - start >= time)
+			break ;
+	}
 }
 
 // philosophers life cycle
@@ -49,11 +49,11 @@ void	*phl_thrd(void	*arg)
 {
 	t_philo	*philo;
 	int		i;
-	
+
 	philo = (t_philo *)arg;
 	philo->tm_start = time_ms();
 	i = 0;
-	while(1)
+	while (1)
 	{
 		ft_printf_out(time_ms() - philo->tm_start, philo, "is eating");
 		ft_msleep(philo->args->t2eat_p);
@@ -71,10 +71,10 @@ void	*phl_thrd(void	*arg)
 t_args	*args_init(t_args *args)
 {
 	int			i;
-	
-	args->numbr_p = 5;			// number_of_philosophers
-	args->t2eat_p = 200; 		// time_to_eat_in_ms
-	args->t2slp_p = 200; 		// time_to_sleep_in_ms
+
+	args->numbr_p = 5;
+	args->t2eat_p = 200;
+	args->t2slp_p = 200;
 	args->times_p = 5;
 	args->fork = calloc(args->numbr_p, sizeof(int));
 	if (!args->fork)
@@ -83,7 +83,7 @@ t_args	*args_init(t_args *args)
 	if (!args->fork_m)
 		return (free(args->fork), NULL);
 	if (pthread_mutex_init(&args->print_mid, NULL))
-		return (free(args->fork),free(args->fork_m), NULL);
+		return (free(args->fork), free(args->fork_m), NULL);
 	i = -1;
 	while (++i < args->numbr_p)
 	{
@@ -92,15 +92,15 @@ t_args	*args_init(t_args *args)
 			while (--i >= 0)
 				pthread_mutex_destroy(&args->fork_m[i]);
 			pthread_mutex_destroy(&args->print_mid);
-			return (free(args->fork), free(args->fork_m), NULL); 
+			return (free(args->fork), free(args->fork_m), NULL);
 		}
 	}
 	return (args);
 }
 
-void args_destroy(t_args *args)
+void	args_destroy(t_args *args)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	while (++i < args->numbr_p)
@@ -116,7 +116,7 @@ int	main(void)
 	int			i;
 	t_philo		*philo;
 
-	if (!args_init(&args)) // struct args initialization
+	if (!args_init(&args))
 		return (1);
 	philo = calloc(args.numbr_p, sizeof(t_philo));
 	if (!philo)
@@ -127,9 +127,9 @@ int	main(void)
 	{
 		philo[i].id = i;
 		philo[i].args = &args;
-		if (pthread_create(&philo[i].thread_id, NULL, phl_thrd, 
-			(void *) &philo[i]))
-				return (args_destroy(&args), free(philo), 4);
+		if (pthread_create(&philo[i].thread_id, NULL, phl_thrd,
+				(void *) &philo[i]))
+			return (args_destroy(&args), free(philo), 4);
 	}
 	i = -1;
 	while (++i < args.numbr_p)
@@ -137,7 +137,7 @@ int	main(void)
 		pthread_join(philo[i].thread_id, NULL);
 		printf("Thread of philosopher %d is joined\n", i);
 	}
-	args_destroy(&args); // struct args destruction
-	free(philo);
+	args_destroy (&args);
+	free (philo);
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 16:39:54 by oruban            #+#    #+#             */
-/*   Updated: 2024/04/12 20:45:15 by oruban           ###   ########.fr       */
+/*   Updated: 2024/04/13 10:08:05 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <unistd.h> // sleep
 # include <stdlib.h> // malloc, free
 # include <string.h> // strerror
-#include <sys/time.h>	// gettimeofday
+# include <sys/time.h>	// gettimeofday
 
 typedef struct s_args
 {
@@ -26,18 +26,18 @@ typedef struct s_args
 	int				t2slp_p; // time_to_sleep_in_ms
 	int				times_p; //number_of_times_each_philosopher_must_eat
 	/// CREATED BUT STILL NOT USED!!!!: 
-	int				*fork;	 // the array of forks may == 0 or 1 only. Needed 
+	int				*fork;	// the array of forks may == 0 or 1 only. Needed 
 	// because U "should protect the forks state with a mutex for each of them."
 	/// CREATED BUT STILL NOT USED!!!!: 
-	pthread_mutex_t	*fork_m;	 // an array of mutexes (fork[numbr_p]) 4 every fork
+	pthread_mutex_t	*fork_m; // an array of mutexes (fork[numbr_p]) 4 every fork
 	pthread_mutex_t	print_mid; // mutex id 2 garantee no data race during printf
 }				t_args;
 
 typedef struct s_philo
 {
-	int			id; 		// id of the philosopher
-	long int	tm_start; 	// start time of the philosophers thread
-	pthread_t	thread_id; 	// thread id of the philosopher
+	int			id;			// id of the philosopher
+	long int	tm_start;	// start time of the philosophers thread
+	pthread_t	thread_id;	// thread id of the philosopher
 	t_args		*args;		// pointer to the arguments structure
 }				t_philo;
 
