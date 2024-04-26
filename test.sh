1a706d28c178e994e7cc4c4a6d68f64228f72c20
Oleh Ruban <oruban@c2s19d121.42wolfsburg.de>
2024-04-26 18:36:49 +0200

    eval tests added 2 test.sh 5 800 200 200 does not work forever

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 
 #!/bin/bash
 
-# ./philo 1 1010 500 500
+# ./philo 1 800 200 200
 
 # leaks --atExit -- ./philo 4 310 200 100
 for i in {1..10}
@@ -14,15 +14,21 @@ do
     # ./philo 4 290 200 100 | grep died
 # the cases below, when the 2 philo simultaneous death can be explained 
 # with PC performance - they r so seldom
-    # ./philo 4 310 200 100 | grep died # 1 per 10
-    # ./philo 4 390 200 100 | grep died
-    # ./philo 5 490 200 100 | grep died  # 1 per 20
-    # ./philo 5 590 200 100 | grep died  # 1 per 30
+    # ./philo 4 310 200 100 | grep died
+    # ./philo 4 390 200 100 | grep died  # 1 per 10
+    # ./philo 5 490 200 100 | grep died  # 1 per 10
+    # ./philo 5 590 200 100 | grep died  # 1 per 10
 done
     # everlasting loops: 
-    # ./philo 200 260 100 100 # 89k
+    # ./philo 200 260 100 100 # 223k
 # the above given subject demand sets the DELTA of project PARAMETERS sensibility to 60 ms
 # therefoe the following tests are too strict and not relevant:
     # ./philo 200 1010 500 500 # 259k 4 died - 2k (((
-    # ./philo 200 410 200 200 # 16k 30, 181 died - 4k - 1k
+    # ./philo 200 410 200 200 # 16k 30, 181 died - 4k - 1k <1k
     # ./philo 200 210 100 100 # 2k 64 died - 1k
+
+# eval tests
+# ./philo 5 800 200 200 7 # ok
+# ./philo 5 800 200 200 8
+# ./philo 4 410 200 200 # ok
+# ./philo 4 310 200 100 # 1 in 10 case 2 die
